2057ae63c905a1c2b1bb7337f5ca7cc85ccd4017
andreyait <andreyivo.ink@gmail.com>
2025-03-18 18:07:55 -0300

    segundo commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
-
+import { PainelPrincipalComponent } from './componentes/painel-principal/painel-principal.component';
+import { CadastroProdutoComponent } from
+'./componentes/cadastro-produto/cadastro-produto.component';
+const routes: Routes = [
+{ path: 'painel-principal', component: PainelPrincipalComponent },
+{ path: 'cadastro-produto', component:
+CadastroProdutoComponent }, // Cadastro de novo produto
+{ path: 'cadastro-produto/:id', component:
+CadastroProdutoComponent }, // Edição de produto
+{ path: '', redirectTo: '/painel-principal', pathMatch: 'full' } //Redirecionamento padrão
+];
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+imports: [RouterModule.forRoot(routes)],
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
