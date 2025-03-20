4288ffa9abbfe38197d7cf64b1ea5f91c8316c70
andreyait <andreyivo.ink@gmail.com>
2025-03-20 20:42:43 -0300

    modificação do app.component.html2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PainelPrincipalComponent } from './componentes/painel-principal/painel-principal.component';
-import { CadastroProdutoComponent } from
-'./componentes/cadastro-produto/cadastro-produto.component';
+import { CadastroProdutoComponent } from './componentes/cadastro-produto/cadastro-produto.component';
 const routes: Routes = [
 { path: 'painel-principal', component: PainelPrincipalComponent },
 { path: 'cadastro-produto', component:
